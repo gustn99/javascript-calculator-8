43c8ddb5582d9bb1f171683e71f1728db0376776
gustn99 <azerosong@gmail.com>
2025-12-31 17:45:55 +0900

    feat: 뼈대 구성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
   async run() {
+    const userInput = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+    const numbers = this.parseNumbers(userInput);
+
+    const result = numbers.reduce((total, cur) => total + cur, 0);
+    Console.print(`결과 : ${result}`);
+  }
+
+  parseNumbers(userInput) {
+    // user input으로부터 식과 구분자 추출 -> numbers 배열 반환
+    return [];
   }
 }
 
