876da43330b033e7a432f27a246ad3b26f930c80
gustn99 <azerosong@gmail.com>
2025-12-31 17:36:25 +0900

    chore: 초기 환경으로 롤백

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import CalculatorController from './controllers/CalculatorController.js';
-
 class App {
   async run() {
-    const calculator = new CalculatorController();
-    await calculator.run();
   }
 }
 
