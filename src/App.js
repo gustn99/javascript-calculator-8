4a88f3a9bd110ead3b94a9db4b2ad2e5f6382906
gustn99 <azerosong@gmail.com>
2025-12-31 18:08:53 +0900

    feat: 파싱 로직 구현

--- src/App.js
+++ src/App.js
@@ -11,8 +11,30 @@ class App {
 
   parseNumbers(userInput) {
     // user input으로부터 식과 구분자 추출 -> numbers 배열 반환
-    return [];
+    const regex = /\/\/(.*?)\\n/g;
+    const matchResults = [...userInput.matchAll(regex)];
+
+    let expression = userInput;
+    let delimiter = ',:';
+
+    if (matchResults.length > 1) {
+      throw new Error('[ERROR] 커스텀 구분자는 한 번만 선언할 수 있습니다.');
+    }
+
+    if (matchResults.length === 1) {
+      const regex = /^(\/\/.*?\\n)(.*)/;
+      const groups = userInput.match(regex);
+
+      expression = groups[2];
+      delimiter = matchResults[0][1];
+    }
+
+    // number 검증
+    const numbers = expression.split(new RegExp(`[${delimiter}]`)).map(Number);
+
+    return numbers;
   }
+
 }
 
 export default App;
